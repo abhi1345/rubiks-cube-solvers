3fe2ecd56ced6ecaef7749ff54a1527361161239
Abhishek Sharma <abhisheks@berkeley.edu>
2025-12-03 11:33:52 -0800

    Add Rubik's cube color constants
    Introduces lists and mappings for Rubik's cube colors, their indices, and their opposites for use throughout the codebase.

---
+++ rubiks/constants.py
@@ -0,0 +1,21 @@
+colors = ["white", "yellow", "red", "orange", "blue", "green"]
+
+color_map = {
+    "white": 0,
+    "yellow": 1,
+    "red": 2,
+    "orange": 3,
+    "blue": 4,
+    "green": 5,
+}
+
+opposite_color_map = {
+    "white": "yellow",
+    "yellow": "white",
+    "red": "orange",
+    "orange": "red",
+    "blue": "green",
+    "green": "blue",
+}
+
+__all__ = ["colors", "color_map", "opposite_color_map"]
